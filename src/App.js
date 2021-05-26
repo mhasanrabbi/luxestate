2abc7551e37bd8c28ec81134983ea232927f14cb
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-05-26 21:31:25 +0600

    minor issue fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from './Components/Navbar';
+import NavBar from './Components/Navbar/Navbar';
 
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <NavBar/>
     </>
   );
 }
