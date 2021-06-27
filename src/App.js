5cdaabb06a28ec884357d6a5840dc7631c72d373
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-06-27 21:34:23 +0600

    rebuild components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router } from 'react-router-dom';
-import Hero from './Components/Hero';
 
 function App() {
   return (
     <Router>
-      <Hero />
+      <App />
     </Router>
   );
 }
